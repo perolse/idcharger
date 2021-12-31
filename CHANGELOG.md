e3ead0286fff34eb907300f581fe3072dd6926ca
Per Olsson <pero67@gmail.com>
2021-12-31 14:35:25 +0100

    Use of refresh token on Id charger API

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # Changelog Home Assistant VW Id Charger addon
 
+## 0.0.4
+
+**New features:**
+
+- Use of refresh token on Id charger API
+
+**Fixed bugs:**
+
+- None
+
 ## 0.0.3
 
 **New features:**
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import signal
 import time
 import logging
+import uuid
+import paho.mqtt.client as mqtt
 from idcharger import IdCharger
 from settings import Settings
 
 settings_filename = '/data/options.json'
-
 
 class Killer:
     kill_now = False
@@ -18,6 +19,62 @@ class Killer:
         self.kill_now = True
 
 
+class Mqtt_manager:
+    def __init__(self, settings):
+        self.settings = settings
+        logging.info("Starting MQTT client")
+        client_id = 'VWIdCharger_' + uuid.uuid4().hex
+        self.mqttClient = mqtt.Client(client_id=client_id)
+        self.mqttClient.on_connect = self.on_connect
+        if len(self.settings.mqtt_user + self.settings.mqtt_password) > 0:
+            self.mqttClient.username_pw_set(
+                username=self.settings.mqtt_user,
+                password=self.settings.mqtt_password)
+
+    def on_connect(self, client, userdata, flags, rc):
+        logging.info("Connected with result code "+str(rc))
+        self.connected_result = rc
+
+    def mqtt_connect(self):
+        logging.info("Connecting to MQTT client")
+        self.mqttClient.connect(self.settings.mqtt_broker,
+                                self.settings.mqtt_port)
+        self.mqttClient.loop(20)
+        return self.connected_result == 0
+
+    def update_sensor_configs(self):
+        config_address = 'homeassistant/sensor/vwidcharger/ct{id}/config'
+        config_template = '{{\n' + \
+            '  "device_class": "current",\n' + \
+            '  "name": "VW ID Charger Ct-Coil-{id}",\n' + \
+            '  "unique_id": "ct{id}",\n' + \
+            '  "state_topic": "vw/idcharger/ctcoil",\n' + \
+            '  "unit_of_measurement": "A",\n' + \
+            '  "value_template": "{{{{ value_json.CT{id} }}}}"\n' + \
+            '}}'
+        result = self.mqttClient.publish(config_address.format(id='1'),
+                                         config_template.format(id='1'))
+        retval = result[0]
+        result = self.mqttClient.publish(config_address.format(id='2'),
+                                         config_template.format(id='2'))
+        retval = retval + result[0]
+        result = self.mqttClient.publish(config_address.format(id='3'),
+                                         config_template.format(id='3'))
+        retval = retval + result[0]
+        return retval == 0
+
+    def send_values(self, ct1, ct2, ct3):
+        value_str = '{{\n' + \
+                    '  "CT1":{val1},\n' + \
+                    '  "CT2":{val2},\n' + \
+                    '  "CT3":{val3}\n' + \
+                    '}}'
+        result = self.mqttClient.publish('vw/idcharger/ctcoil',
+                                         value_str.format(val1=ct1,
+                                                          val2=ct2,
+                                                          val3=ct3))
+        return result[0] == 0
+
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
@@ -26,18 +83,21 @@ if __name__ == '__main__':
     killer = Killer()
     settings = Settings(settings_filename)
     id_charger = IdCharger(settings)
-    if not id_charger.mqtt_connect():
+    mqtt_manager = Mqtt_manager(settings)
+    if not mqtt_manager.mqtt_connect():
         logging.error("Couldn't connect to MQTT broker")
         exit()
-    if not id_charger.update_sensor_configs():
+    if not mqtt_manager.update_sensor_configs():
         logging.error("Couldn't update sensor configs")
         exit()
     while True:
         if id_charger.fetch_values():
-            if id_charger.send_values():
+            if mqtt_manager.send_values(id_charger.ct1, id_charger.ct2, id_charger.ct3):
                 logging.info("Send values")
             else:
-                logging.warn("Couldn't send values")
+                logging.warning("Couldn't send values")
+                mqtt_manager.mqtt_connect()
         time.sleep(10)
         if killer.kill_now:
+            id_charger.stop()
             break
